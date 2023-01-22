af7c2f62dfbc8372095a85a6a1f3bc8e6e7c7da3
Сергей Козлов <sergejkozlov@MacBook-Pro-Sergej.local>
2023-01-22 14:29:00 +0400

    first commit

---
+++ Bitsgap/Settings.swift
@@ -0,0 +1,31 @@
+import WebKit
+
+struct Cookie {
+    var name: String
+    var value: String
+}
+
+let gcmMessageIDKey = "00000000000" // update this with actual ID if using Firebase
+
+// URL for first launch
+let rootUrl = URL(string: "https://twa.bitsgap.com/bot?utm_source=app_bound_domain")!
+
+// allowed origin is for what we are sticking to pwa domain
+// This should also appear in Info.plist
+let allowedOrigins: [String] = ["bitsgap.com"]
+
+// auth origins will open in modal and show toolbar for back into the main origin.
+// These should also appear in Info.plist
+let authOrigins: [String] = ["facebook.com"]
+// allowedOrigins + authOrigins <= 10
+
+// subdomains that are allowed, but must be forced to open in the popup
+let allowedOriginsForcedPopupMode: [String] = ["helpdesk.bitsgap.com"];
+
+let platformCookie = Cookie(name: "app-platform", value: "iOS App Store")
+
+// UI options
+let displayMode = "standalone" // standalone / fullscreen.
+let adaptiveUIStyle = true     // iOS 15+ only. Change app theme on the fly to dark/light related to WebView background color.
+let overrideStatusBar = false   // iOS 13-14 only. if you don't support dark/light system theme.
+let statusBarTheme = "dark"    // dark / light, related to override option.
